a988903fa9794ed1612337f31beaccf2d8de9761
ifef228 <ifef228@gmail.com>
2024-08-05 11:25:08 +0300

    cherry-peak

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,7 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Открытие магазина ожидается через 4 дня");
+        System.out.println("cherry-peak");
     }
 }
 
